1c4375180504fda8a9860f11bea753da8839c244
Zalonics <anthony.dev.9898@gmail.com>
2022-06-02 15:37:00 -0700

    AoC day 9

--- AoC/2021/9/2.js
+++ AoC/2021/9/2.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
-const grid = fs.readFileSync('./input.txt', 'utf-8').split('\n')
+
+const grid = fs
+    .readFileSync('./input.txt', 'utf-8')
+    .split('\n')
+    .map((line) => line.split('').map((num) => +num))
 
 const isUpValid = (y, x) => {
     if (y === 0) {
@@ -28,6 +32,7 @@ const isLeftValid = (y, x) => {
     }
     return grid[y][x - 1] > grid[y][x] ? true : false
 }
+
 const isLowPoint = (y, x) => {
     if (!isUpValid(y, x)) return false
     if (!isDownValid(y, x)) return false
@@ -36,69 +41,85 @@ const isLowPoint = (y, x) => {
     return true
 }
 
-const lowPoints = []
-for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[0].length; x++) {
-        if (isLowPoint(y, x)) {
-            lowPoints.push([y, x])
+const getLowPoints = () => {
+    const lowPoints = []
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[0].length; x++) {
+            if (isLowPoint(y, x)) {
+                lowPoints.push([y, x])
+            }
         }
     }
+    return lowPoints
 }
-console.log(lowPoints)
 
-const visitAllNeighbors = (y, x) => {
-    let output = []
-    let currentPoint = +grid[y][x]
-    let candidate
-
-    const isValid = (currentPoint, candidate) => {
-        if (candidate > currentPoint && candidate !== 9) {
+const getBasinSizeGivenPoint = (y, x) => {
+    const visited = new Set()
+    const queue = []
+    queue.push([y, x])
+    while (queue.length !== 0) {
+        let [y, x] = queue[0]
+        // Queue up Neighbors
+        const isBeingVisited = (y, x) => {
+            if (grid[y][x] === 9) {
+                return false
+            }
+            if (visited.has(`${y},${x}`)) {
+                return false
+            }
             return true
         }
-    }
+        const checkTopNeighbor = (y, x) => {
+            if (y === 0) {
+                return
+            }
+            if (isBeingVisited(y - 1, x)) {
+                queue.push([y - 1, x])
+                visited.add(`${y},${x}`)
+            }
+        }
+        const checkBottomNeighbor = (y, x) => {
+            if (y === grid.length - 1) {
+                return
+            }
+            if (isBeingVisited(y + 1, x)) {
+                visited.add(`${y},${x}`)
+                queue.push([y + 1, x])
+            }
+        }
+        const checkRightNeighbor = (y, x) => {
+            if (x === grid[0].length - 1) {
+                return
+            }
+            if (isBeingVisited(y, x + 1)) {
+                queue.push([y, x + 1])
+            }
+        }
+        const checkLeftNeighbor = (y, x) => {
+            if (x === 0) {
+                return
+            }
+            if (isBeingVisited(y, x - 1)) {
+                queue.push([y, x - 1])
+            }
+        }
+        visited.add(`${y},${x}`)
 
-    //Down
-    if (y !== grid.length - 1) {
-        candidate = +grid[y + 1][x]
-        if (isValid(currentPoint, candidate)) output.push([y + 1, x])
+        checkTopNeighbor(y, x)
+        checkBottomNeighbor(y, x)
+        checkLeftNeighbor(y, x)
+        checkRightNeighbor(y, x)
+        // Dequeue Initial Point and
+        queue.shift()
     }
-    //Up
-    if (y !== 0) {
-        candidate = +grid[y - 1][x]
-        if (isValid(currentPoint, candidate)) output.push([y - 1, x])
-    }
-    //Right
-    if (x !== grid[0].length - 1) {
-        candidate = +grid[y][x + 1]
-        if (isValid(currentPoint, candidate)) output.push([y, x + 1])
-    }
-    //Left
-    if (x !== 0) {
-        candidate = +grid[y][x - 1]
-        if (isValid(currentPoint, candidate)) output.push([y, x - 1])
-    }
-    return output
+    return [...visited].length
 }
 
-// const visited = new Set()
-
-// for (let y = 0; y < grid.length; y++) {
-//     for (let x = 0; x < grid[0].length; x++) {
-//         if (!visited.has(`${y},${x}`) && grid[y][x] != '9') {
-//             let size = 0
-//             let queue = []
-//             queue.push(`${y},${x}`)
-//             while (queue.length !== 0) {
-//                 if (visited.has(`${y},${x}`)) {
-//                     continue
-//                 }
-//                 visited.add(`${y},${x}`)
-//                 size++
-//                 const neighbors = visitAllNeighbors(y, x)
-//                 neighbors.forEach(([y,x]) => {
-//                     if
-//                 })
-//             }
-//         }
-//     }
-// }
+const lowPoints = getLowPoints()
+const basins = lowPoints
+    .reduce((acc, [y, x]) => {
+        return [...acc, getBasinSizeGivenPoint(y, x)]
+    }, [])
+    .sort((a, b) => b - a)
+const [a, b, c] = basins
+console.log(a * b * c)
